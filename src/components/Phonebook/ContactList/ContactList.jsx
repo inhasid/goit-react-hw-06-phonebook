1fc624c4fff5beec1db76ad2ca59d9e59de47252
inhasid <sidorchuk.inga@gmail.com>
2024-02-02 21:23:02 +0100

    commit3

--- src/components/Phonebook/ContactList/ContactList.jsx
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -1,14 +1,17 @@
-import styles from "./contact-list.module.css";
+import styles from './contact-list.module.css';
 
 const ContactList = ({ items, deleteContact }) => {
-    const elements = items.map(({ id, name, number }) =>
-        <li key={id}> {name}: {number}  <button onClick={()=> deleteContact(id)} type="button">Delete</button></li>)
+  const elements = items.map(({ id, name, number }) => (
+    <li key={id}>
+      {' '}
+      {name}: {number}{' '}
+      <button onClick={() => deleteContact(id)} type="button">
+        Delete
+      </button>
+    </li>
+  ));
 
-    return (
-            <ul className={styles.list}>
-                {elements}
-            </ul>
-    )
-}
+  return <ul className={styles.list}>{elements}</ul>;
+};
 
 export default ContactList;
