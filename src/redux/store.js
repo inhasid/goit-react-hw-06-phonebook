08566284509856425cc33c77f641203e11ab1dc0
inhasid <sidorchuk.inga@gmail.com>
2024-02-05 09:29:04 +0100

    Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-import rootReducer from './rootReducer';
+import persistedReducer from './rootReducer';
 
-const store = configureStore({
-  reducer: rootReducer,
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default store;
+export const persistor = persistStore(store);
